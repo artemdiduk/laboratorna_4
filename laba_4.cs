366af4c543af8e06ea8ddcf421dd99d47f0dad1e
artemdiduk <68499262+artemdiduk@users.noreply.github.com>
2020-12-01 01:04:59 +0200

    Create laba_4.cs

---
+++ laba_4.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace lb4task1
+{ 
+  class ArrayExample
+  {
+    static void Main()
+    {
+        char[] letters = { 'a', 'r', 't', 'e', 'm',  ' ', 'd', 'i', 'd', 'y', 'k',};
+        string name = "";
+        int[] a = new int[19];
+        for (int i = 0; i < letters.Length; i++)
+        {
+            name += letters[i];
+            a[i] = i + 1;
+            SendMessage(name, a[i]);
+        }
+        Console.ReadKey();
+    }
+    static void SendMessage(string name, int msg)
+    {
+        Console.WriteLine("Hello, " + name + "! Count to " + msg);
+    }
+  }
